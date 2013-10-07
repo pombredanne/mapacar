b0003860fff5d05d617be4b47647d278ba6d3177
Ed Summers <ehs@pobox.com>
2013-10-07 07:05:55 -0400

    updated csv

--- makecsv.py
+++ makecsv.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-"""
-generate a csv similar to the table that is available at 
-http://monitoramento.sema.mt.gov.br/navegador_mapa_lau/MapaLau.html
-"""
-
 import os
 import unicodecsv
 
 from lxml import etree
 
 output = unicodecsv.writer(open("data.csv", "wb"), encoding="utf-8")
-output.writerow([u"Propriedade", u"Municipio", u"Lau Emitido", u"Car Emitido",
-u"Proprietários", "uProcessos", u"CodAprt", u"Shape"])
+output.writerow(["ProcessoNumero", "Interessado", "Nome/Razao", "Municipio", "Area APRT", "Perimetro APRT", "Shape"])
 
 for dirpath, dirnames, filenames in os.walk("data"):
     for f in filenames:
@@ -24,14 +17,12 @@ for dirpath, dirnames, filenames in os.walk("data"):
         metadata_xml = os.path.join(dirpath, f)
         item = etree.parse(metadata_xml)
         row = []
-        row.append(item.find('./{http://tempuri.org/}Id').text)
-        row.append(item.find('./{http://tempuri.org/}Propriedade').text)
+        row.append(item.find('./{http://tempuri.org/}ProcessoNumero').text)
+        row.append(item.find('./{http://tempuri.org/}ProcessoInteressado').text)
+        row.append(item.find('./{http://tempuri.org/}NomeRazao').text)
         row.append(item.find('./{http://tempuri.org/}Municipio').text)
-        row.append(item.find('./{http://tempuri.org/}Lau').text)
-        row.append(item.find('./{http://tempuri.org/}Car').text)
-        row.append(item.find('./{http://tempuri.org/}Proprietarios').text)
-        row.append(item.find('./{http://tempuri.org/}Processos').text)
-        row.append(item.find('./{http://tempuri.org/}CodAprt').text)
+        row.append(item.find('./{http://tempuri.org/}AreaAPRT').text)
+        row.append(item.find('./{http://tempuri.org/}PerimetroAPRT').text)
 
         shape = metadata_xml.replace(".xml", ".zip")
         if os.path.isfile(shape):
